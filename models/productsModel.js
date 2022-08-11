7f5c34cd319cd77eb91bf9d7b6cf3929b3fc1b11
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-11 18:44:17 -0300

    feat: altera retorno de getProductById

--- models/productsModel.js
+++ models/productsModel.js
@@ -9,7 +9,7 @@ const productsModel = {
   },
   getProductById: async (id) => {
     const query = 'SELECT id, name FROM StoreManager.products WHERE id = ?;';
-    const [[products]] = await connection.execute(query, [id]);
+    const [products] = await connection.execute(query, [id]);
 
     return products;
   },
