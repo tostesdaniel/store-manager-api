25e5222f0e522052fa235eeb92f221ddac6d9cd4
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-11 23:16:00 -0300

    feat: adds productsRouter

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const productsRouter = require('./routes/productsRouter');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use('/products', productsRouter);
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
