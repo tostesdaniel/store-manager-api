1a8079438c9a62da54f4066cb4b4a4df512f73fb
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-11 21:01:56 -0300

    feat: implements productsController

---
+++ controllers/productsController.js
@@ -0,0 +1,20 @@
+const productsService = require('../services/productsService');
+
+const productsController = {
+  getProducts: async (_req, res) => {
+    const products = await productsService.getProducts();
+
+    return res.status(200).json(products);
+  },
+  getProductById: async (req, res) => {
+    const { id } = req.params;
+
+    const [product] = await productsService.getProductById();
+
+    if (!product) return res.status(404).json({ message: 'Product not found' });
+
+    return res.status(200).json(product);
+  },
+};
+
+module.exports = productsController;
