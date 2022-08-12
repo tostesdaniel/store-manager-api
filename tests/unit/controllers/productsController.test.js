24c3a8d11cfba67fd6e9b470d394ffa7a0cfb80a
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-11 22:33:22 -0300

    test: adapts test to suit implementation of productsController

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -1,12 +1,7 @@
 const sinon = require("sinon");
 const { expect } = require("chai");
-// const productsController = require("../../../controllers/productsController");
-// const productsService = require("../../../controllers/productsController");
-
-const productsController = {
-  getProducts: () => {},
-  getProductById: () => {},
-};
+const productsController = require("../../../controllers/productsController");
+const productsService = require("../../../services/productsService");
 
 describe("Testa productsController", () => {
   const allProducts = [
@@ -19,10 +14,14 @@ describe("Testa productsController", () => {
     const req = {};
     const res = {};
 
-    before(() => {
+    before(async () => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
+
+      sinon.stub(productsService, "getProducts").resolves(allProducts);
     });
+
+    after(async () => productsService.getProducts.restore());
 
     it("Verifica se o método status é chamado com código 200", async () => {
       await productsController.getProducts(req, res);
@@ -30,7 +29,7 @@ describe("Testa productsController", () => {
       expect(res.status.calledWith(200)).to.be.true;
     });
 
-    it("Verifica se o método json é chamado com um array", async () => {
+    it("Verifica se o método json é chamado com um array contendo todos os produtos", async () => {
       await productsController.getProducts(req, res);
 
       expect(res.json.calledWith(allProducts)).to.be.true;
@@ -38,14 +37,25 @@ describe("Testa productsController", () => {
   });
 
   describe("Ao listar um produto por ID", () => {
-    const req = {};
-    const res = {};
-
     describe("Caso o produto não exista", () => {
       const notFoundError = {
         code: 404,
         message: "Product not found",
       };
+
+      const req = {};
+      const res = {};
+
+      before(async () => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        req.params = { id: 103254987 };
+
+        sinon.stub(productsService, "getProductById").resolves([]);
+      });
+
+      after(async () => productsService.getProductById.restore());
 
       it("Verifica se o método status é chamado com código 404", async () => {
         await productsController.getProductById(req, res);
@@ -66,6 +76,22 @@ describe("Testa productsController", () => {
         message: allProducts[0],
       };
 
+      const req = {};
+      const res = {};
+
+      before(async () => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        req.params = { id: 1 };
+
+        sinon
+          .stub(productsService, "getProductById")
+          .resolves([allProducts[0]]);
+      });
+
+      after(async () => productsService.getProductById.restore());
+
       it("Verifica se o método status é chamado com código 200", async () => {
         await productsController.getProductById(req, res);
 
@@ -75,8 +101,7 @@ describe("Testa productsController", () => {
       it("Verifica se o método json é chamado com um array contendo o objeto do produto", async () => {
         await productsController.getProductById(req, res);
 
-        expect(res.json.calledWith({ message: goodResponse.message })).to.be
-          .true;
+        expect(res.json.calledWith(goodResponse.message)).to.be.true;
       });
     });
   });
