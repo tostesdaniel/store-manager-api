433f274d00c8c53584fc3b7bc01b0863021de79a
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-12 19:48:23 -0300

    feat: changes return type

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,7 +3,7 @@ module.exports = (err, _req, res, _next) => {
   if (err.message) {
     const [error, code] = err.message.split('|');
 
-    return res.status(Number(code)).json(error);
+    return res.status(Number(code)).json({ message: error });
   }
 
   return res.status(500).json({ message: 'Internal error' });
