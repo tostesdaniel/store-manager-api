2c54fd39aef249c4ec57dfa86ed4d40d4c1dc3d2
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-12 15:44:42 -0300

    feat: error now returns status code at the end of the msg

--- services/validations/productValidations.js
+++ services/validations/productValidations.js
@@ -3,8 +3,8 @@ const Joi = require('joi');
 const schema = {
   createProduct: Joi.object({
     name: Joi.string().min(5).required().messages({
-      'string.min': '"name" length must be at least 5 characters long',
-      'any.required': '"name" is required',
+      'string.min': '"name" length must be at least 5 characters long|422',
+      'any.required': '"name" is required|400',
     }),
   }),
 };
