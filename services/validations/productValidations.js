1df2f9c940beed668455a35bcdcdbee77e5e8fae
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-13 19:10:01 -0300

    feat: update product validations

--- services/validations/productValidations.js
+++ services/validations/productValidations.js
@@ -7,6 +7,12 @@ const schema = {
       'any.required': '"name" is required|400',
     }),
   }),
+  updateProduct: Joi.object({
+    name: Joi.string().min(5).required().messages({
+      'string.min': '"name" length must be at least 5 characters long|422',
+      'any.required': '"name" is required|400',
+    }),
+  }),
 };
 
 module.exports = schema;
