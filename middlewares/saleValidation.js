5ce05172adc8637cdecd80062f0d7d384e98b394
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 17:05:43 -0300

    feat: sale body validation middleware

---
+++ middlewares/saleValidation.js
@@ -0,0 +1,17 @@
+const validations = require('../services/validations/saleValidations');
+
+const saleValidation = (req, _res, next) => {
+  const sales = req.body;
+  const input = sales.map((sale) => validations.createSale.validate(sale));
+  let validSales = [...input];
+
+  input.forEach((sale) => {
+    if (sale.error) {
+      validSales = { message: sale.error.details[0].message };
+    }
+  });
+
+  return validSales.message ? next(validSales) : next();
+};
+
+module.exports = saleValidation;
