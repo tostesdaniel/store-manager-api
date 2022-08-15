c81c5f9131da6772aaa1f01974575aad8ca87831
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 19:29:39 -0300

    test: adjustments to suit function implementation

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -1,7 +1,7 @@
-// const sinon = require("sinon");
+const sinon = require("sinon");
 const { expect } = require("chai");
-// const salesService = require("../../../controllers/salesController");
-// const salesController = require("../../../controllers/salesController");
+const salesService = require("../../../services/salesService");
+const salesController = require("../../../controllers/salesController");
 
 describe("Testa salesController", () => {
   describe("Ao cadastrar uma venda", () => {
@@ -12,21 +12,21 @@ describe("Testa salesController", () => {
     ];
     const notFoundResponse = { message: "Product not found|404" };
 
-    const salesController = {
-      create: () => {},
-    };
-
     describe("Caso o produto não exista no banco de dados", () => {
       const req = {};
       const res = {};
       let next;
 
       before(() => {
-        req.body = [...noProductIdSaleBody];
+        req.body = noProductIdSaleBody;
         res.status = res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
         next = sinon.stub().returns();
+
+        sinon.stub(salesService, "create").resolves(notFoundResponse);
       });
+
+      after(() => salesService.create.restore());
 
       it("Verifica se next é chamado com objeto de erro", async () => {
         await salesController.create(req, res, next);
@@ -41,11 +41,17 @@ describe("Testa salesController", () => {
       let next;
 
       before(() => {
-        req.body = [...goodSaleBody];
+        req.body = goodSaleBody;
         res.status = res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
         next = sinon.stub().returns();
+
+        sinon
+          .stub(salesService, "create")
+          .resolves({ id: 3, itemsSold: goodSaleBody });
       });
+
+      after(() => salesService.create.restore());
 
       it("Verifica se status é chamado com status 201", async () => {
         await salesController.create(req, res, next);
