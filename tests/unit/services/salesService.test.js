9f32b89b6f3cf2bdcdf79f81c59282027efbda5e
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 18:45:24 -0300

    test: adjustments to suit function implementation

--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -1,7 +1,7 @@
-// const sinon = require("sinon");
+const sinon = require("sinon");
 const { expect } = require("chai");
-// const salesModel = require("../../../models/salesModel");
-// const salesService = require('../../../services/salesService')
+const salesModel = require("../../../models/salesModel");
+const salesService = require("../../../services/salesService");
 
 describe("Testa salesService", () => {
   describe("Ao cadastrar uma venda", () => {
@@ -11,11 +11,18 @@ describe("Testa salesService", () => {
       { productId: 2, quantity: 2 },
     ];
     const notFoundResponse = { message: "Product not found|404" };
-    const salesService = {
-      create: () => {},
-    };
 
     describe("Caso o produto não exista no banco de dados", () => {
+      before(async () => {
+        sinon.stub(salesModel, "create").resolves();
+        sinon.stub(salesModel, "createLink").resolves(true);
+      });
+
+      after(async () => {
+        salesModel.create.restore();
+        salesModel.createLink.restore();
+      });
+
       it("Verifica se é retornado um erro", async () => {
         const response = await salesService.create(noProductIdSaleBody);
 
@@ -24,6 +31,16 @@ describe("Testa salesService", () => {
     });
 
     describe("Caso o produto exita no banco de dados", () => {
+      before(async () => {
+        sinon.stub(salesModel, "create").resolves(3);
+        sinon.stub(salesModel, "createLink").resolves(true);
+      });
+
+      after(async () => {
+        salesModel.create.restore();
+        salesModel.createLink.restore();
+      });
+
       it("Verifica se é retornado os detalhes da inserção", async () => {
         const response = await salesService.create(goodSaleBody);
 
