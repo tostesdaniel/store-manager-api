b3a9e1e0fd50bac3962030af8e8d743acc8f1137
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 20:53:49 -0300

    test: base tests for salesModel get and getById

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -10,7 +10,7 @@ describe("Testa salesModel", () => {
     );
 
     after(async () => connection.execute.restore());
-    
+
     it("Verifica se é retornado o id da venda ao criar uma venda em sales", async () => {
       const response = await salesModel.create();
 
@@ -23,4 +23,57 @@ describe("Testa salesModel", () => {
       expect(response).to.equal(true);
     });
   });
+
+  describe('Ao listar vendas', () => {
+    const allSalesResponse = [
+  {
+    sale_id: 1,
+    date: '2022-08-15T23:36:07.000Z',
+    product_id: 1,
+    quantity: 5
+  },
+  {
+    sale_id: 1,
+    date: '2022-08-15T23:36:07.000Z',
+    product_id: 2,
+    quantity: 10
+  },
+  {
+    sale_id: 2,
+    date: '2022-08-15T23:36:07.000Z',
+    product_id: 3,
+    quantity: 15
+  },
+  {
+    sale_id: 3,
+    date: '2022-08-15T23:36:08.000Z',
+    product_id: 1,
+    quantity: 1
+  },
+  {
+    sale_id: 3,
+    date: '2022-08-15T23:36:08.000Z',
+    product_id: 2,
+    quantity: 5
+  }
+]
+    describe("Ao listar todas as vendas", () => {
+      it("Verifica se é possível listar todas as vendas com sucesso", async () => {
+        const response = await salesModel.get();
+
+        expect(response).to.be.deep.equal(allSalesResponse);
+      });
+    });
+
+    describe("Ao listar uma venda", () => {
+      it("Verifica se é possível listar a venda com sucesso", async () => {
+        const response = await salesModel.getById(3);
+
+        expect(response).to.be.deep.equal(allSalesResponse.slice(-2))
+      });
+    });
+  });
+  
+
+  
 });
