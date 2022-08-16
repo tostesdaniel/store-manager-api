e05d50f4b5e026221f859cd706d493675d47e9b6
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-16 15:54:07 -0300

    feat: sales test mocks

---
+++ tests/unit/mocks/sales.js
@@ -0,0 +1,56 @@
+module.exports = {
+  allSalesResponse: [
+    {
+      sale_id: 1,
+      date: "2022-08-16T00:06:39.000Z",
+      product_id: 1,
+      quantity: 5,
+    },
+    {
+      sale_id: 1,
+      date: "2022-08-16T00:06:39.000Z",
+      product_id: 2,
+      quantity: 10,
+    },
+    {
+      sale_id: 2,
+      date: "2022-08-16T00:06:39.000Z",
+      product_id: 3,
+      quantity: 15,
+    },
+  ],
+  formattedSalesResponse: [
+    {
+      saleId: 1,
+      date: "2022-08-16T00:06:39.000Z",
+      productId: 1,
+      quantity: 5,
+    },
+    {
+      saleId: 1,
+      date: "2022-08-16T00:06:39.000Z",
+      productId: 2,
+      quantity: 10,
+    },
+    {
+      saleId: 2,
+      date: "2022-08-16T00:06:39.000Z",
+      productId: 3,
+      quantity: 15,
+    },
+  ],
+  singleSaleResponse: [
+    {
+      date: "2022-08-16T00:06:39.000Z",
+      product_id: 3,
+      quantity: 15,
+    },
+  ],
+  formattedSingleSaleResponse: [
+    {
+      date: "2022-08-16T00:06:39.000Z",
+      productId: 3,
+      quantity: 15,
+    },
+  ],
+};
