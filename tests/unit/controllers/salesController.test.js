607ab53d2801d4c73892fdd25cbbbe217f6b260c
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 22:27:36 -0300

    test: base tests for salesController get and getById

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -67,4 +67,72 @@ describe("Testa salesController", () => {
       });
     });
   });
+
+  describe("Ao listar vendas", () => {
+    describe("Ao listar todas as vendas ", () => {
+      const req = {};
+      const res = {};
+
+      before(() => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+      });
+
+      it("Verifica se status é chamado com código 200", async () => {
+        await salesController.get(req, res);
+
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+
+      it("Verifica se json é chamado com todas as vendas", async () => {
+        const response = await salesController.get(req, res);
+
+        expect(res.json.calledWith(response)).to.be.true;
+      });
+    });
+
+    describe("Ao listar uma venda que não existe", () => {
+      const req = {};
+      const res = {};
+      let next;
+
+      before(() => {
+        req.params = { id: 1999 };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+        next = sinon.stub().returns();
+      });
+
+      it("Verifica se next é chamado com a mensagem de erro", async () => {
+        await salesController.get(req, res, next);
+
+        expect(next.calledWith({ message: "Sale not found" })).to.be.true;
+      });
+    });
+
+    describe("Ao listar uma venda que existe", () => {
+      const req = {};
+      const res = {};
+      let next;
+
+      before(() => {
+        req.params = { id: 2 };
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+        next = sinon.stub().returns();
+      });
+
+      it("Verifica se status é chamado com código 200", async () => {
+        await salesController.get(req, res, next);
+
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+
+      it("Verifica se json é chamado com a venda buscada", async () => {
+        const response = await salesController.get(req, res, next);
+
+        expect(res.json.calledWith(response)).to.be.true;
+      });
+    });
+  });
 });
