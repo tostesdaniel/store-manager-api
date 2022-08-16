fedc2cd9431d77ac1f02c81db33317244f6f1945
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 21:29:43 -0300

    test: adjustments to suit function implementation

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -24,40 +24,34 @@ describe("Testa salesModel", () => {
     });
   });
 
-  describe('Ao listar vendas', () => {
+  describe("Ao listar vendas", () => {
     const allSalesResponse = [
-  {
-    sale_id: 1,
-    date: '2022-08-15T23:36:07.000Z',
-    product_id: 1,
-    quantity: 5
-  },
-  {
-    sale_id: 1,
-    date: '2022-08-15T23:36:07.000Z',
-    product_id: 2,
-    quantity: 10
-  },
-  {
-    sale_id: 2,
-    date: '2022-08-15T23:36:07.000Z',
-    product_id: 3,
-    quantity: 15
-  },
-  {
-    sale_id: 3,
-    date: '2022-08-15T23:36:08.000Z',
-    product_id: 1,
-    quantity: 1
-  },
-  {
-    sale_id: 3,
-    date: '2022-08-15T23:36:08.000Z',
-    product_id: 2,
-    quantity: 5
-  }
-]
+      {
+        sale_id: 1,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 1,
+        quantity: 5,
+      },
+      {
+        sale_id: 1,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 2,
+        quantity: 10,
+      },
+      {
+        sale_id: 2,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 3,
+        quantity: 15,
+      },
+    ];
     describe("Ao listar todas as vendas", () => {
+      before(async () =>
+        sinon.stub(connection, "execute").resolves([allSalesResponse])
+      );
+
+      after(async () => connection.execute.restore());
+
       it("Verifica se é possível listar todas as vendas com sucesso", async () => {
         const response = await salesModel.get();
 
@@ -66,14 +60,17 @@ describe("Testa salesModel", () => {
     });
 
     describe("Ao listar uma venda", () => {
-      it("Verifica se é possível listar a venda com sucesso", async () => {
-        const response = await salesModel.getById(3);
+      before(async () =>
+        sinon.stub(connection, "execute").resolves([allSalesResponse.slice(-1)])
+      );
 
-        expect(response).to.be.deep.equal(allSalesResponse.slice(-2))
+      after(async () => connection.execute.restore());
+
+      it("Verifica se é possível listar a venda com sucesso", async () => {
+        const response = await salesModel.getById(2);
+
+        expect(response).to.be.deep.equal([allSalesResponse[2]]);
       });
     });
   });
-  
-
-  
 });
