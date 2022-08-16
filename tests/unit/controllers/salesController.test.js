8a0d15203b4026652559e1ec6f3070e47e764714
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-15 22:45:28 -0300

    test: adjustments to suit function implementation

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -69,6 +69,27 @@ describe("Testa salesController", () => {
   });
 
   describe("Ao listar vendas", () => {
+    const allSalesResponse = [
+      {
+        sale_id: 1,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 1,
+        quantity: 5,
+      },
+      {
+        sale_id: 1,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 2,
+        quantity: 10,
+      },
+      {
+        sale_id: 2,
+        date: "2022-08-16T00:06:39.000Z",
+        product_id: 3,
+        quantity: 15,
+      },
+    ];
+
     describe("Ao listar todas as vendas ", () => {
       const req = {};
       const res = {};
@@ -76,7 +97,11 @@ describe("Testa salesController", () => {
       before(() => {
         res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
+
+        sinon.stub(salesService, "get").resolves(allSalesResponse);
       });
+
+      after(() => salesService.get.restore());
 
       it("Verifica se status é chamado com código 200", async () => {
         await salesController.get(req, res);
@@ -85,9 +110,9 @@ describe("Testa salesController", () => {
       });
 
       it("Verifica se json é chamado com todas as vendas", async () => {
-        const response = await salesController.get(req, res);
-
-        expect(res.json.calledWith(response)).to.be.true;
+        await salesController.get(req, res);
+  
+        expect(res.json.calledWith(allSalesResponse)).to.be.true;
       });
     });
 
@@ -101,10 +126,16 @@ describe("Testa salesController", () => {
         res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
         next = sinon.stub().returns();
+
+        sinon
+          .stub(salesService, "getById")
+          .resolves({ message: "Sale not found" });
       });
 
+      after(() => salesService.getById.restore());
+
       it("Verifica se next é chamado com a mensagem de erro", async () => {
-        await salesController.get(req, res, next);
+        await salesController.getById(req, res, next);
 
         expect(next.calledWith({ message: "Sale not found" })).to.be.true;
       });
@@ -120,18 +151,24 @@ describe("Testa salesController", () => {
         res.status = sinon.stub().returns(res);
         res.json = sinon.stub().returns();
         next = sinon.stub().returns();
+
+        sinon
+          .stub(salesService, "getById")
+          .resolves(allSalesResponse.slice(-1));
       });
 
+      after(() => salesService.getById.restore());
+
       it("Verifica se status é chamado com código 200", async () => {
-        await salesController.get(req, res, next);
+        await salesController.getById(req, res, next);
 
         expect(res.status.calledWith(200)).to.be.true;
       });
 
       it("Verifica se json é chamado com a venda buscada", async () => {
-        const response = await salesController.get(req, res, next);
+        await salesController.getById(req, res, next);
 
-        expect(res.json.calledWith(response)).to.be.true;
+        expect(res.json.calledWith([allSalesResponse[2]])).to.be.true;
       });
     });
   });
