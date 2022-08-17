15be9efadc72c807717cdd5b8d06ad33aa38398b
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-16 22:11:47 -0300

    feat: create product mocks

---
+++ tests/unit/mocks/products.js
@@ -0,0 +1,8 @@
+module.exports = {
+  allProducts: [
+    { id: 1, name: "Martelo de Thor" },
+    { id: 2, name: "Traje de encolhimento" },
+    { id: 3, name: "Escudo do Capitão América" },
+  ],
+  oneProduct: [{ id: 3, name: "Escudo do Capitão América" }],
+};
