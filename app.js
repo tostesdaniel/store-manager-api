4e37fca9ab78126682fbd26ceda0c9c5f61bfda1
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-29 16:47:57 -0300

    refactor(app): remove unused code

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const express = require('express');
-const productsRouter = require('./routes/productsRouter');
-const salesRouter = require('./routes/salesRouter');
-const errorMiddleware = require('./middlewares/error');
+const express = require("express");
+const productsRouter = require("./routes/productsRouter");
+const salesRouter = require("./routes/salesRouter");
+const errorMiddleware = require("./middlewares/error");
 
 const app = express();
 app.use(express.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
+app.use("/products", productsRouter);
 
-app.use('/products', productsRouter);
-
-app.use('/sales', salesRouter);
+app.use("/sales", salesRouter);
 
 app.use(errorMiddleware);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
