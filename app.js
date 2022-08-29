7e4a552d5856f8382b58c6dbde5612147dabe3d5
Daniel Tostes <ohtosttnz@gmail.com>
2022-08-29 16:50:27 -0300

    feat(app): swagger docs endpoint

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const productsRouter = require("./routes/productsRouter");
 const salesRouter = require("./routes/salesRouter");
 const errorMiddleware = require("./middlewares/error");
 
 const app = express();
 app.use(express.json());
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/products", productsRouter);
 
